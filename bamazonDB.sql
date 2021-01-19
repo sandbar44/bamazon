340bafdbd309ef73e6842ee94365cc84c49cd8a2
Sandra <sandbar44x@gmail.com>
2021-01-19 15:02:20 -0500

    SQL script

--- bamazonDB.sql
+++ bamazonDB.sql
@@ -2,6 +2,7 @@ DROP DATABASE IF EXISTS bamazon;
 CREATE DATABASE bamazon;
 USE bamazon;
 
+DROP TABLE products;
 CREATE TABLE products (
   item_id INTEGER AUTO_INCREMENT NOT NULL,
   product_name VARCHAR(30) NOT NULL,
@@ -12,43 +13,45 @@ CREATE TABLE products (
   PRIMARY KEY (item_id)
 );
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Playing Cards", "Toys & Games", 5, 525);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Playing Cards", "Toys & Games", 5, 525, 15000);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Sequence Game", "Toys & Games", 24.99, 125);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Sequence Game", "Toys & Games", 24.99, 485, 60000);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Cards Against Humanity", "Toys & Games", 25, 204);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Cards Against Humanity", "Toys & Games", 25, 112, 25000);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Smart Home Camera", "Camera & Photo", 25.98, 34);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Smart Home Camera", "Camera & Photo", 25.98, 34, 13000);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Outdoor Security Camera", "Camera & Photo", 39.98, 87);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Outdoor Security Camera", "Camera & Photo", 39.98, 87, 150000);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Baby Monitor", "Camera & Photo", 49.99, 165);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Baby Monitor", "Camera & Photo", 49.99, 165, 204000);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("1984 by George Orwell", "Books", 17.89, 24);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("1984 by George Orwell", "Books", 17.89, 24, 12000);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Bridgerton The Duke & I", "Books", 13.22, 89);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Bridgerton The Duke & I", "Books", 13.22, 89, 5000);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("The Ender Quartet Boxed Set", "Books", 34.96, 51);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("The Ender Quartet Boxed Set", "Books", 34.96, 51, 12000);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Reusable Face Mask", "Clothing", 6.99, 846);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Reusable Face Mask", "Clothing", 6.99, 846, 9000);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Women's Yoga Pants", "Clothing", 29.99, 65);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Women's Yoga Pants", "Clothing", 29.99, 65, 11000);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Lover by Taylor Swift", "Music", 19.99, 99);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES ("Lover by Taylor Swift", "Music", 19.99, 99, 19000);
 
 SELECT * FROM products;
+
+///////////////////////////////////////////////////////////////////////////////////-- 
 
 DROP TABLE departments;
 CREATE TABLE departments (
@@ -75,12 +78,6 @@ VALUES ("Music",20000);
 
 SELECT * FROM departments;
 
-SELECT d.*, sum(p.product_sales) as product_sales, (p.product_sales-d.over_head_costs) as total_profit
+SELECT d.*, sum(p.product_sales) as product_sales, (sum(p.product_sales)-d.over_head_costs) as total_profit
 FROM departments d LEFT JOIN products p ON d.department_name = p.department_name
 GROUP BY d.department_name,d.over_head_costs;
-
-ALTER TABLE products
-ADD product_sales INTEGER NOT NULL;
-
-ALTER TABLE products
-MODIFY COLUMN product_sales DECIMAL(10,2) NOT NULL;
